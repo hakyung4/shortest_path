355aee937ccf941c8789ed4d4b5ec5a4c05c4a73
hakyung4 <hakyung4@illinois.edu>
2022-04-27 01:35:00 -0500

    tried build a graph constructor

--- Graph.h
+++ Graph.h
@@ -1,15 +1,31 @@
-// #include <algorithm>
-// #include <fstream>
-// #include <iomanip>
-// #include <iostream>
-// #include <list>
-// #include <sstream>
-// #include <string>
-// #include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
 
-// class Graph {
-// public:
-//   void addEdge(std::vector<int> adj[], std::string inputFile);
-//   void printGraph(std::vector<int> adj[], int V);
-//   void BFS(std::vector<int> adj[], int s, int target, int V);
-// };
+class Graph {
+public:
+  Graph(std::vector<int> start, std::vector<int> end, std::vector<float> wt);
+  ~Graph();
+  void addEdge(std::vector<int> adj[], std::string inputFile);
+  void printGraph(std::vector<int> adj[], int V);
+  void BFS(std::vector<int> adj[], int s, int target, int V);
+  void addWeight(std::string inputFile, std::vector<int> startNode, std::vector<int> endNode, std::vector<float> weight);
+  void set_startNode(std::vector<int> startNode);
+  void set_endNode(std::vector<int> endNode);
+  void set_weight(std::vector<float> weight);
+  std::vector<int> get_startNode();
+  std::vector<int> get_endNode();
+  std::vector<float> get_weight();
+
+private:
+  int vertex_;
+  std::list<int> *adjList;
+  std::vector<int> startNode_;
+  std::vector<int> endNode_;
+  std::vector<float> weight_;
+};
