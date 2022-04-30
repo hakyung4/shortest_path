7d9be454d9a90160ce8d04fd0a32e1b570c16b21
hakyung4 <hakyung4@illinois.edu>
2022-04-29 22:20:46 -0500

    backup

--- Graph.cpp
+++ Graph.cpp
@@ -1,5 +1,6 @@
 #include "Graph.h"
 
+
 void Graph::set_startNode(std::vector<int> startNode) {
   for (unsigned i = 0; i < startNode.size(); i++) {
     startNode_.push_back(startNode[i]);
@@ -46,7 +47,7 @@ void Graph::addWeight(std::string inputFile, std::vector<int> startNode, std::ve
     weight.push_back(n4);
   }
 
-  for (int i = 0; i < startNode.size(); i++) {
+  for (int i = 0; i < int(startNode.size()); i++) {
     std::cout << "Start, End, Distance : " << std::endl;
     std::cout << startNode[i] << std::endl;
     std::cout << endNode[i] << std::endl;
@@ -73,6 +74,30 @@ void Graph::addEdge(std::vector<int> adj[], std::string inputFile) {
   }
 }
 
+void Graph::addEdgeDijkstra(std::vector<int> adj[], std::string inputFile) {
+    std::ifstream File(inputFile);
+    std::string line;
+    while (std::getline(File, line)) {
+        int temp;
+        int node1;
+        int node2;
+        int weight;
+
+        std::stringstream ss(line);
+        ss >> temp;
+        ss >> node1;
+        ss >> node2;
+        ss >> weight;
+
+        // add to matrix
+        std::pair<auto, auto> pair1(node2, weight);
+        std::pair<auto, auto> pair2(node1, weight);
+
+        adj[node1].push_back(pair1);
+        adj[node2].push_back(pair2);
+    }
+}
+
 // A utility function to print the adjacency list
 // representation of graph
 void Graph::printGraph(std::vector<int> adj[], int V) {
@@ -86,44 +111,101 @@ void Graph::printGraph(std::vector<int> adj[], int V) {
 }
 
 void Graph::BFS(std::vector<int> adj[], int s, int target, int V) {
-  // Mark all the vertices as not visited
-  bool *visited = new bool[V];
-  for (int i = 0; i < V; i++)
-    visited[i] = false;
 
-  // Create a queue for BFS
-  std::list<int> queue;
-  int distance = 0;
+    // Path vector to store directions
+    std::vector<int> path;
 
-  // Mark the current node as visited and enqueue it
-  visited[s] = true;
-  queue.push_back(s);
+    // Store prev cells in a vector, resize, set all to -1
+    std::vector<int> prevCells;
+    prevCells.resize(V, -1);
 
-  // 'i' will be used to get all adjacent
-  // vertices of a vertex
-  std::list<int>::iterator i;
+    // Lengths will allow us to store the different lengths, to be used later
+    // Helped simplify the implementation
+    // resize to size and set all to -1
+    std::vector<int> lengths;
+    lengths.resize(V, 0);
 
-  while (!queue.empty()) {
-    s = queue.front();
-    queue.pop_front();
-    distance++;
+    // Create a queue for BFS
+    std::queue<int> queue;
 
-    // If we found our target destination, return distance
-    if (s == target) {
-      std::cout << std::endl;
-      std::cout << "Target destination found!" << std::endl;
-      std::cout << "Distance = " << distance << std::endl;
-      std::cout << "s = " << s << std::endl;
-      std::cout << "target = " << target << std::endl;
+    // Mark all the vertices as not visited
+    std::vector<bool> visited;
+    visited.resize(V, false);
+
+    // Use a stack to store directions, to be dumped into the path vector later
+    std::stack<int> directions;
+
+    // 'i' will be used to get all adjacent
+    // vertices of a vertex
+    std::list<int>::iterator i;
+
+    // Mark the current node as visited and enqueue it
+    int start = s;
+    int distance = 0;
+    int counter = 0;
+    visited[s] = true;
+    queue.push(s);
+    bool endFlag = false;
+    int shortestLength = V;
+
+    if (start == target) {
+      shortestLength = 0;
+      return;
     }
-    // Get all adjacent vertices of the dequeued
-    // vertex s. If a adjacent has not been visited,
-    // then mark it visited and enqueue it
-    for (auto x : adj[s]) {
-      if (!visited[int(x)]) {
-        visited[int(x)] = true;
-        queue.push_back(x);
-      }
+
+    while(!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        queue.pop();
+    
+
+        // If we found our target destination, return distance
+        if (s == target) {
+          std::cout << std::endl;
+          std::cout << "Target destination found!" << std::endl;
+          std::cout << "Distance = " << distance - 1 << std::endl;
+          std::cout << "start = " << start << std::endl;
+          std::cout << "curPointer s = " << s << std::endl;
+          std::cout << "target = " << target << std::endl;
+          endFlag = true;
+        }
+
+ 
+        // Get all adjacent vertices of the dequeued
+        // vertex s. If a adjacent has not been visited,
+        // then mark it visited and enqueue it
+
+        for (auto x : adj[s]) {
+
+            if (!visited[int(x)]) {
+            visited[int(x)] = true;
+            queue.push(x);
+
+            // Print paths for debugging
+            lengths[int(x)] = lengths[int(s)] + 1;
+            prevCells[int(x)] = s;
+
+            std::cout << "length at node " << x << " = "<< lengths[int(x)] << std::endl;
+            }
+        }
     }
-  }
+
+    int prev = prevCells[target];
+    int cur = target;
+    while (prev != -1) {
+      distance++;
+      cur = prev;
+      prev = prevCells[cur];
+      path.push_back(cur);
+    }
+
+    std::cout << "\n Shortest Path backtrack: \n " << target;
+    for (auto x : path) {
+        std::cout << " -> " << x;
+    }
+
+    std::cout << "\n Length = " << distance << std::endl;
+    std::cout << "\n counter = " << counter << std::endl;
+
 }
--- Graph.h
+++ Graph.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <algorithm>
 #include <fstream>
 #include <iomanip>
@@ -6,19 +7,27 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <queue>
+
 
 class Graph {
 public:
-  void addEdge(std::vector<int> adj[], std::string inputFile);
   void printGraph(std::vector<int> adj[], int V);
-  void BFS(std::vector<int> adj[], int s, int target, int V);
-  void addWeight(std::string inputFile, std::vector<int> startNode, std::vector<int> endNode, std::vector<float> weight);
   void set_startNode(std::vector<int> startNode);
   void set_endNode(std::vector<int> endNode);
   void set_weight(std::vector<float> weight);
+  void addWeight(std::string inputFile, std::vector<int> startNode, std::vector<int> endNode, std::vector<float> weight);
   std::vector<int> get_startNode();
   std::vector<int> get_endNode();
   std::vector<float> get_weight();
+
+  // BFS
+  void addEdge(std::vector<int> adj[], std::string inputFile);
+  void BFS(std::vector<int> adj[], int s, int target, int V);
+
+  // Dijkstra
+  void addEdgeDijkstra(std::vector<int> adj[], std::string inputFile);
+  void Dijkstra(std::vector<int> adj[], int s, int target, int V);
 
 private:
   int vertex_;
